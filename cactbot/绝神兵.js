2b85a6c9da08c73129deace0ea9b453ea7258bf1
leifengsang <575617231@qq.com>
2022-12-12 17:30:11 +0800

    实现基础需求，未测试

--- cactbot/绝神兵.js
+++ cactbot/绝神兵.js
@@ -1,3 +1,7 @@
+/**
+ * 绝神兵 三连桶
+ */
+
 Options.Triggers.push({
     zoneId: ZoneId.TheWeaponsRefrainUltimate,
     triggers: [
@@ -5,20 +9,42 @@ Options.Triggers.push({
             id: "leilei 大怒震",
             netRegex: NetRegexes.startsUsing({ id: "2B67" }),
             run: (data) => {
-                data.markCount = 0; //石牢点名count
+                data.markList = []; //初始化石牢点名列表
+                data.leileiData.leileiFL.clearMark();
             }
         },
         {
-            id: "leilei 石牢点名",
+            id: "leilei 三连桶石牢点名",
             netRegex: NetRegexes.ability({ id: "2B6[BC]" }),
-            run: (data, matches) => {
+            condition: (data, matches) => {
                 //大怒震后的前三个石牢点名为三连桶点名
-                if (data.markCount < 3) {
-                    //TODO 等custom库先写完吧
+                return data.markList.length < 3;
+            },
+            run: (data, matches) => {
+                data.markList.push({
+                    id: matches.targetId,
+                    name: matches.target,
+                    job: data.party.details.find((v) => {
+                        return v.id == matches.targetId;
+                    })?.job,
+                });
 
-                    data.markCount++;
+                //点名到达三个，开始标记
+                if (data.markList.length == 3) {
+                    const compareFunction = (obj) => {
+                        return data.leileiData.myParty.findIndex((v) => {
+                            return v.id == obj.id;
+                        });
+                    }
+                    data.markList.sort((a, b) => {
+                        return compareFunction(a) - compareFunction(b);
+                    });
+
+                    data.leileiData.leileiFL.mark(data.markList[0].id, data.leileiData.targetMakers.attack1);
+                    data.leileiData.leileiFL.mark(data.markList[1].id, data.leileiData.targetMakers.attack2);
+                    data.leileiData.leileiFL.mark(data.markList[2].id, data.leileiData.targetMakers.attack3);
                 }
             }
-        }
+        },
     ]
 })
