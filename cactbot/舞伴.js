cf9758b0435a6cb11354f322749731163e200770
leifengsang <575617231@qq.com>
2022-12-12 23:44:10 +0800

    喊话内容可配置化

--- cactbot/舞伴.js
+++ cactbot/舞伴.js
@@ -8,20 +8,15 @@ Options.Triggers.push({
         {
             id: "leilei world 1st dnc's macro",
             netRegex: NetRegexes.ability({ id: "3E86" }),
-            condition: (data, matches, output) => {
-                if(output.enable !== "true"){
-                    return false;
-                }
-
+            condition: (data, matches) => {
                 //自己对他人释放
                 return matches.source === data.me;
             },
-            run: (data, matches) => {
-                doTextCommand("/p 谁敢向我挑衅，我将终结他的生命！" + matches.target
-                    + "《国一" + data.leileiFL.getJobNameByName(data, matches.target, "single") + "5%伤害》符文已配！")
+            run: (data, matches, output) => {
+                data.leileiFL.doTextCommand(output.content({ name: matches.target, job: data.leileiFL.getJobNameByHexId(data, matches.targetId, "single") }));
             },
             outputStrings: {
-                enable: true
+                content: "/p 谁敢向我挑衅，我将终结他的生命！《${name}国一${job}5%伤害》符文已配！"
             },
         }
     ]
