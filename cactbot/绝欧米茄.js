0fbe14a0f46f9938f1aa5db12322b241f1ebf700
leifengsang <575617231@qq.com>
2023-01-25 21:34:02 +0800

    增加P2一运头顶标记（默认关闭）

--- cactbot/绝欧米茄.js
+++ cactbot/绝欧米茄.js
@@ -5,6 +5,19 @@
 const MAHJONG_PHASE_P1_1 = 1; //P1 拉线踩塔
 const MAHJONG_PHASE_P1_2 = 2; //P1 全能之主
 
+const headMarker = {
+    programPTCircle: "01A0",
+    programPTTriangle: "01A1",
+    programPTSquare: "01A2",
+    programPTX: "01A3",
+}
+
+const firstDecimalMarker = parseInt("0017", 16);
+const getHeadmarkerId = (data, matches) => {
+    if (!data.leileiDecOffset) data.leileiDecOffset = parseInt(matches.id, 16) - firstDecimalMarker;
+    return (parseInt(matches.id, 16) - data.leileiDecOffset).toString(16).toUpperCase().padStart(4, "0");
+};
+
 Options.Triggers.push({
     zoneId: ZoneId.TheOmegaProtocolUltimate,
     initData: () => {
@@ -14,18 +27,21 @@ Options.Triggers.push({
             p1_2_mahjongCount: 0,
             p1_2_myMahjongGroup: 0,
             p2_programPT_TTS_Dic: { "M": "", "F": "" },
+            p2_programPT_markReverse: false,
+            p2_programPT_markCount: 0,
+            p2_programPT_groupDic: { "circle": [], "triangle": [], "square": [], "x": [] },
         }
     },
     triggers: [
         {
-            id: "leilei p1 循环编译",
+            id: "leilei TOP p1 循环编译",
             netRegex: NetRegexes.startsUsing({ id: "7B03" }),
             run: (data) => {
                 data.mahjongPhase++;
             }
         },
         {
-            id: "leilei p1 拉线",
+            id: "leilei TOP p1 拉线",
             //BBC 1号
             //BBD 2号
             //BBE 3号
@@ -63,7 +79,7 @@ Options.Triggers.push({
             }
         },
         {
-            id: "leilei p1 踩塔",
+            id: "leilei TOP p1 踩塔",
             //BBC 1号
             //BBD 2号
             //BBE 3号
@@ -101,14 +117,14 @@ Options.Triggers.push({
             }
         },
         {
-            id: "leilei p1 全能之主",
+            id: "leilei TOP p1 全能之主",
             netRegex: NetRegexes.startsUsing({ id: "7B0B" }),
             run: (data) => {
                 data.mahjongPhase++;
             }
         },
         {
-            id: "leilei p1 诱导导弹P",
+            id: "leilei TOP p1 诱导导弹P",
             netRegex: NetRegexes.gainsEffect({ effectId: ["D60", "DA7", "DA8", "DA9"] }),
             condition: (data, matches) => {
                 return matches.target === data.me;
@@ -124,7 +140,7 @@ Options.Triggers.push({
             }
         },
         {
-            id: "leilei p1 全能之主分组",
+            id: "leilei TOP p1 全能之主分组",
             //BBC 1号
             //BBD 2号
             //BBE 3号
@@ -161,8 +177,8 @@ Options.Triggers.push({
                     return;
                 }
 
-                let list = data.p1_2_mahjongGroupDic[data.p1_2_myMahjongGroup];
-                let targetName = list.find((v) => {
+                const list = data.p1_2_mahjongGroupDic[data.p1_2_myMahjongGroup];
+                const targetName = list.find((v) => {
                     return v !== data.me;
                 });
 
@@ -173,7 +189,7 @@ Options.Triggers.push({
             }
         },
         {
-            id: "leilei p2 一运男人读条",
+            id: "leilei TOP p2 一运男人读条",
             //7B25 月环
             //7B26 钢铁
             netRegex: NetRegexes.startsUsing({ id: ["7B25", "7B26"] }),
@@ -202,7 +218,7 @@ Options.Triggers.push({
             }
         },
         {
-            id: "leilei p2 一运女人读条",
+            id: "leilei TOP p2 一运女人读条",
             //7B2A 辣翅
             //7B2D 辣尾
             netRegex: NetRegexes.startsUsing({ id: ["7B2A", "7B2D"] }),
@@ -223,6 +239,107 @@ Options.Triggers.push({
             outputStrings: {
                 辣翅: "去中间",
                 辣尾: "去两边"
+            }
+        },
+        {
+            id: "leilei TOP Headmarker Tracker",
+            netRegex: NetRegexes.headMarker({}),
+            condition: (data) => undefined === data.leileiDecOffset,
+            run: (data, matches) => {
+                getHeadmarkerId(data, matches);
+            },
+        },
+        {
+            id: "leilei TOP p2 靠近远离",
+            //D63 靠近
+            //D64 远离
+            netRegex: NetRegexes.gainsEffect({ effectId: ["D63", "D64"] }),
+            condition: (data, matches) => {
+                return matches.target === data.me;
+            },
+            run: (data, matches) => {
+                data.p2_programPT_markReverse = matches.effectId === "D64";
+            }
+        },
+        {
+            id: "leilei TOP p2 头顶标记标点",
+            netRegex: NetRegexes.headMarker({}),
+            run: (data, matches, output) => {
+                if (output.是否标记() !== "true") {
+                    return;
+                }
+                const id = getHeadmarkerId(data, matches);
+                const headMarkers = [
+                    headMarker.programPTCircle,
+                    headMarker.programPTTriangle,
+                    headMarker.programPTSquare,
+                    headMarker.programPTX,
+                ]
+                if (!headMarkers.includes(id)) {
+                    return;
+                }
+
+                let psType = "";
+                switch (id) {
+                    case headMarker.programPTCircle:
+                        psType = "circle";
+                        break;
+                    case headMarker.programPTTriangle:
+                        psType = "triangle";
+                        break;
+                    case headMarker.programPTSquare:
+                        psType = "square";
+                        break;
+                    case headMarker.programPTX:
+                        psType = "x";
+                        break;
+                    default:
+                        break;
+                }
+                data.p2_programPT_groupDic[psType].push(data.leileiFL.getRpByHexId(data, matches.targetId));
+                data.p2_programPT_markCount++;
+                if (data.p2_programPT_markCount != 8) {
+                    return;
+                }
+
+                //正序优先级：圆圈 叉 方块 三角
+                let rpRuleList = output.优先级().split("/");
+                for (const key in data.p2_programPT_groupDic) {
+                    let list = data.p2_programPT_groupDic[key];
+                    list.sort((a, b) => {
+                        return rpRuleList.indexOf(a) - rpRuleList.indexOf(b);
+                    })
+                }
+
+                let highGroup = [
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["circle"][0]),
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["x"][0]),
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["square"][0]),
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["triangle"][0]),
+                ];
+                let lowGroup = [
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["circle"][1]),
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["x"][1]),
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["square"][1]),
+                    data.leileiFL.getHexIdByRp(data, data.p2_programPT_groupDic["triangle"][1]),
+                ];
+                if (data.p2_programPT_markReverse) {
+                    lowGroup.reverse();
+                }
+
+                data.leileiFL.mark(highGroup[0], data.leileiData.targetMakers.attack1);
+                data.leileiFL.mark(highGroup[1], data.leileiData.targetMakers.attack2);
+                data.leileiFL.mark(highGroup[2], data.leileiData.targetMakers.attack3);
+                data.leileiFL.mark(highGroup[3], data.leileiData.targetMakers.attack4);
+
+                data.leileiFL.mark(lowGroup[0], data.leileiData.targetMakers.bind1);
+                data.leileiFL.mark(lowGroup[1], data.leileiData.targetMakers.bind2);
+                data.leileiFL.mark(lowGroup[2], data.leileiData.targetMakers.bind3);
+                data.leileiFL.mark(lowGroup[3], data.leileiData.targetMakers.attack5);
+            },
+            outputStrings: {
+                优先级: "H1/MT/ST/D1/D2/D3/D4/H2",
+                是否标记: "false"
             }
         },
     ]
