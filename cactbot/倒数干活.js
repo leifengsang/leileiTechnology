3d10fc647cf2d5e3e9de19e5cebc0e87fa2c4ad1
leifengsang <575617231@qq.com>
2023-10-18 20:26:26 +0800

    为了防止自己倒数开小差，写一些倒数的自动化猴戏

---
+++ cactbot/倒数干活.js
@@ -0,0 +1,58 @@
+/**
+ * 倒数的时候干一些活
+ */
+
+const regex = /距离战斗开始还有(?<time>\d{1,2})秒！.+?/;
+Options.Triggers.push({
+    zoneId: ZoneId.MatchAll,
+    initData: () => {
+        return {
+            canceled: false
+        };
+    },
+    triggers: [
+        {
+            id: "leilei countdown start blm",
+            netRegex: NetRegexes.gameLog({ line: regex, capture: true }),
+            condition: (data, matches) => {
+                return data.job === "BLM";
+            },
+            preRun: (data) => {
+                data.canceled = false;
+            },
+            delaySeconds: (data, matches) => {
+                return Math.max(0, parseInt(matches.time) - 27);
+            },
+            run: (data, matches) => {
+                if (!data.canceled) {
+                    data.leileiFL.doTextCommand("/ac 激情咏唱");
+                }
+            },
+        },
+        {
+            id: "leilei countdown start rdm",
+            netRegex: NetRegexes.gameLog({ line: regex, capture: true }),
+            condition: (data, matches) => {
+                return data.job === "RDM";
+            },
+            preRun: (data) => {
+                data.canceled = false;
+            },
+            delaySeconds: (data, matches) => {
+                return Math.max(0, parseInt(matches.time) - 5.2);
+            },
+            run: (data, matches) => {
+                if (!data.canceled) {
+                    data.leileiFL.doTextCommand("/ac 赤暴风");
+                }
+            },
+        },
+        {
+            id: "leilei countdown canceled",
+            netRegex: NetRegexes.gameLog({ line: /.+?取消了战斗开始倒计时。|.+?により、戦闘開始カウントがキャンセルされました。|Countdown canceled by .+?./ }),
+            run: (data) => {
+                data.canceled = true;
+            },
+        },
+    ]
+})
