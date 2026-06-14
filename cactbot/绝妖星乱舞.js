b517d131801f2c31262fd8a2ae74c42c3dc296b9
leifengsang <575617231@qq.com>
2026-06-15 01:26:07 +0800

    P4全套（除最后真假雷）

--- cactbot/绝妖星乱舞.js
+++ cactbot/绝妖星乱舞.js
@@ -56,6 +56,13 @@ Options.Triggers.push({
             p2_round: 0,
             p2_endCount: 0,
             p3_stage1_buffType: 0,
+            p4_exdeathStatus: true,
+            p4_chaosStatus: true,
+            p4_isAllagan: true,
+            p4_isPurple: true,
+            p4_eyesTip: {},
+            p4_stayTip: {},
+            p4_eyesSolved: {},
         }
     },
     config: [
@@ -70,6 +77,21 @@ Options.Triggers.push({
                 cn: "开启全局标记",
                 en: "开启全局标记",
                 jp: "开启全局标记",
+            },
+            type: "checkbox",
+            default: false
+        },
+        {
+            id: "p4_show_party",
+            comment: {
+                cn: "需同时使用雷雷科技通用库.js",
+                en: "需同时使用雷雷科技通用库.js",
+                jp: "需同时使用雷雷科技通用库.js",
+            },
+            name: {
+                cn: "开启P4小队播报",
+                en: "开启P4小队播报",
+                jp: "开启P4小队播报",
             },
             type: "checkbox",
             default: false
@@ -152,7 +174,7 @@ Options.Triggers.push({
         },
         {
             id: "leilei DMU 控制战斗阶段",
-            netRegex: NetRegexes.startsUsing({ id: ["BCF2", "BABC", "BAE2", "C2D2"] }),
+            netRegex: NetRegexes.startsUsing({ id: ["BCF2", "BABC", "BAE2", "C2DC"] }),
             run: (data, matches) => {
                 switch (matches.id) {
                     case "BCF2":
@@ -167,7 +189,7 @@ Options.Triggers.push({
                         //重构
                         data.phase = PHASE_EXDEATH_AND_CHAOS;
                         break;
-                    case "C2D2":
+                    case "C2DC":
                         //闹哄哄魂击
                         data.phase = PHASE_KEFKA_SAYS;
                         break;
@@ -836,21 +858,238 @@ Options.Triggers.push({
                 "content": "动动动"
             }
         },
-        // {
-        //     id: "leilei MDU p4 水buff",
-        //     netRegex: NetRegexes.gainsEffect({ effectId: "15A9" }),
-        //     delaySeconds: (data, matches) => {
-        //         return parseInt(matches.duration) - 5;
-        //     },
-        //     condition: (data, matches) => {
-        //         return matches.target === data.me;
-        //     },
-        //     infoText: (data, matches, output) => {
-        //         return output.content();
-        //     },
-        //     outputStrings: {
-        //         "假": "出人群",
-        //     }
-        // },
+        {
+            id: "leilei MDU p4 真假新生艾克斯迪司",
+            netRegex: NetRegexes.gainsEffect({ effectId: "808", count: ["461", "462"] }),
+            run: (data, matches) => {
+                data.p4_exdeathStatus = parseInt(matches.count) == 462;
+            }
+        },
+        {
+            id: "leilei MDU p4 真假卡奥斯",
+            netRegex: NetRegexes.gainsEffect({ effectId: "808", count: ["460", "45F"] }),
+            run: (data, matches) => {
+                data.p4_chaosStatus = parseInt(matches.count) == 460;
+            }
+        },
+        {
+            id: "leilei MDU p4 假水buff 上buff提醒",
+            netRegex: NetRegexes.gainsEffect({ effectId: "15A9" }),
+            condition: (data, matches) => {
+                return !data.p4_exdeathStatus && matches.target === data.me;
+            },
+            durationSeconds: 10,
+            infoText: (data, matches, output) => {
+                const round = parseInt(matches.duration) === 51 ? 1 : 2;
+                return output.content({ round: round });
+            },
+            outputStrings: {
+                content: "第${round}轮出人群"
+            }
+        },
+        {
+            id: "leilei MDU p4 假水buff 判定前播报",
+            netRegex: NetRegexes.gainsEffect({ effectId: "15A9" }),
+            delaySeconds: (data, matches) => {
+                return parseInt(matches.duration) - 5;
+            },
+            durationSeconds: 10,
+            condition: (data, matches) => {
+                return !data.p4_exdeathStatus && matches.target === data.me;
+            },
+            infoText: (data, matches, output) => {
+                return output.content();
+            },
+            outputStrings: {
+                content: "出人群"
+            }
+        },
+        {
+            id: "leilei MDU p4 真雷buff 上buff提醒",
+            netRegex: NetRegexes.gainsEffect({ effectId: "15A8" }),
+            condition: (data, matches) => {
+                return data.p4_exdeathStatus && matches.target === data.me;
+            },
+            durationSeconds: 10,
+            infoText: (data, matches, output) => {
+                const round = parseInt(matches.duration) === 51 ? 1 : 2;
+                return output.content({ round: round });
+            },
+            outputStrings: {
+                content: "第${round}轮出人群"
+            }
+        },
+        {
+            id: "leilei MDU p4 真雷buff 判定前播报",
+            netRegex: NetRegexes.gainsEffect({ effectId: "15A8" }),
+            delaySeconds: (data, matches) => {
+                return parseInt(matches.duration) - 5;
+            },
+            durationSeconds: 10,
+            condition: (data, matches) => {
+                return data.p4_exdeathStatus && matches.target === data.me;
+            },
+            infoText: (data, matches, output) => {
+                return output.content();
+            },
+            outputStrings: {
+                content: "出人群"
+            }
+        },
+        {
+            id: "leilei MDU p4 石化buff 小队播报",
+            netRegex: NetRegexes.gainsEffect({ effectId: "15A7" }),
+            suppressSeconds: 1,
+            condition: (data, matches) => {
+                return data.triggerSetConfig.p4_show_party;
+            },
+            run: (data, matches, output) => {
+                const round = parseInt(matches.duration) === 60 ? 1 : 2;
+                const option = data.p4_exdeathStatus ? "石化出, 背对" : "石化进, 正对";
+                data.leileiFL.doTextCommand(output.content({ round: round, option: option }));
+            },
+            outputStrings: {
+                content: "/p 第${round}轮${option}<se.1>"
+            }
+        },
+        {
+            id: "leilei MDU p4 石化buff 判定前播报",
+            netRegex: NetRegexes.gainsEffect({ effectId: "15A7" }),
+            delaySeconds: (data, matches) => {
+                return parseInt(matches.duration) - 5;
+            },
+            durationSeconds: 10,
+            preRun: (data, matches, output) => {
+                let option;
+                if (matches.target === data.me) {
+                    option = data.p4_exdeathStatus ? "去人群背后背对另外一人" : "去人群前面看另外一人";
+                    data.p4_eyesTip[matches.duration] = output.content({ option: option });
+                }
+                if (!data.p4_eyesTip[matches.duration]) {
+                    option = data.p4_exdeathStatus ? "背对，石化出" : "正对，石化进";
+                    data.p4_eyesTip[matches.duration] = output.content({ option: option });
+                }
+            },
+            infoText: (data, matches) => {
+                if (data.p4_eyesSolved[matches.duration]) {
+                    return;
+                }
+
+                data.p4_eyesSolved[matches.duration] = true;
+                return data.p4_eyesTip[matches.duration];
+            },
+            outputStrings: {
+                content: "${option}"
+            }
+        },
+        {
+            id: "leilei MDU p4 动静剑 判定前播报",
+            netRegex: NetRegexes.gainsEffect({ effectId: "15AA" }),
+            delaySeconds: (data, matches) => {
+                return parseInt(matches.duration) - 5;
+            },
+            durationSeconds: 10,
+            condition: (data, matches) => {
+                return matches.target === data.me;
+            },
+            preRun: (data, matches, output) => {
+                data.p4_moveTip = data.p4_exdeathStatus ? output.真() : output.假();
+            },
+            infoText: (data) => {
+                return data.p4_moveTip;
+            },
+            outputStrings: {
+                "真": "停停停",
+                "假": "动动动"
+            }
+        },
+        {
+            id: "leilei MDU p4 混沌之炎/水 小队播报",
+            //15AB:混沌之炎, 15AC:混沌之水
+            netRegex: NetRegexes.gainsEffect({ effectId: ["15AB", "15AC"] }),
+            suppressSeconds: 1,
+            condition: (data, matches) => {
+                return data.triggerSetConfig.p4_show_party;
+            },
+            run: (data, matches, output) => {
+                const round = (parseInt(matches.duration) === 60 || parseInt(matches.duration) === 45) ? 1 : 2;
+                /**
+                 * 混沌之水 === 真卡奥斯 || 混沌之火 === 假卡奥斯
+                 * 这两种情况需要留原地，否则跑出去
+                 */
+                let stay = (matches.effectId === "15AC") === data.p4_chaosStatus;
+                const option = stay ? "月环" : "钢铁";
+                data.leileiFL.doTextCommand(output.content({ round: round, option: option }));
+            },
+            outputStrings: {
+                content: "/p 第${round}轮${option}<se.1>"
+            }
+        },
+        {
+            id: "leilei MDU p4 混沌之炎/水 判定前播报",
+            //15AB:混沌之炎, 15AC:混沌之水
+            netRegex: NetRegexes.gainsEffect({ effectId: ["15AB", "15AC"] }),
+            delaySeconds: (data, matches) => {
+                return parseInt(matches.duration) - 5;
+            },
+            durationSeconds: 10,
+            suppressSeconds: 1,
+            preRun: (data, matches, output) => {
+                /**
+                 * 混沌之水 === 真卡奥斯 || 混沌之火 === 假卡奥斯
+                 * 这两种情况需要留原地，否则跑出去
+                 */
+                let stay = (matches.effectId === "15AC") === data.p4_chaosStatus;
+                const option = stay ? "月环" : "钢铁";
+                data.p4_stayTip[matches.effectId] = output.content({ option: option });
+            },
+            infoText: (data, matches) => {
+                return data.p4_stayTip[matches.effectId];
+            },
+            outputStrings: {
+                content: "去中间放${option}"
+            }
+        },
+        {
+            id: "leilei MDU p4 记录死超/亚拉戈",
+            //566|1558:死超, 1C6:亚拉戈
+            netRegex: NetRegexes.gainsEffect({ effectId: ["1558", "566", "1C6"] }),
+            condition: (data, matches) => {
+                return matches.target === data.me;
+            },
+            run: (data, matches, output) => {
+                data.p4_isAllagan = matches.effectId === "1C6";
+            }
+        },
+        {
+            id: "leilei MDU p4 记录鸳鸯锅颜色",
+            //15A5|1317:紫(生者之伤), 15A6|1318:蓝(死者之伤)
+            netRegex: NetRegexes.gainsEffect({ effectId: ["15A5", "15A6", "1317", "1318"] }),
+            condition: (data, matches) => {
+                return matches.target === data.me;
+            },
+            run: (data, matches, output) => {
+                data.p4_isPurple = matches.effectId === "15A5" || matches.effectId === "1317";
+            }
+        },
+        {
+            id: "leilei MDU p4 鸳鸯锅播报",
+            netRegex: NetRegexes.startsUsing({ id: "C394" }),
+            infoText: (data, matches, output) => {
+                /**
+                 * 真+亚拉戈 || 假+死超
+                 * 这两种情况需要去反色，否则去同色
+                 */
+                const reverse = data.p4_exdeathStatus === data.p4_isAllagan;
+                let isPurple = data.p4_isPurple;
+                if (reverse) {
+                    isPurple = !isPurple;
+                }
+                return output.content({ color: isPurple ? "紫色" : "蓝色" });
+            },
+            outputStrings: {
+                content: "去${color}"
+            }
+        },
     ]
 })
